2d6f7ea9b30475de2c5af03ab17a1e3ee0e41ca7
Falk Zorn <falkzorn@gmail.com>
2024-03-27 11:49:51 +0100

    Added Bezirk borders and hover Added more Categories and Colors

--- map_test.js
+++ map_test.js
@@ -1,50 +1,44 @@
 var map = L.map('map').setView([52.52, 13.405], 13); // Setzen Sie die Karte auf Berlin
 
+var geojsonBezirke
+
 var geoJsonMarkerOptions = {
+    color: 'black',
     radius: 8,
     weight: 1,
     opacity: 1,
     clickable: true,
-    fillOpacity: .5
+    fillOpacity: 1
 }
 
-// Fügen Sie eine Basiskarte hinzu
+// Hinzufügen der Basiskarte
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
+// Erstellen des Besizrksoverlays
+fetch('https://raw.githubusercontent.com/funkeinteraktiv/Berlin-Geodaten/master/berlin_bezirke.geojson')
+    .then(response => response.json())
+    .then(data => {
+        geojsonBezirke = packageBezirke(data).addTo(map);
+    })
+    .catch(function(error) {
+        console.log(`Error processing Bezirk data: ${error}`)
+    })
+
 // Definieren Sie Ihre Layer-Gruppen mit individuellen Farben
 var layerGroups = {
-    "City-Light-Poster": {
-        group: L.layerGroup(),
-        data: [],
-        color: '#ef476f'
-    },
-    "City-Light-Board": {
-        group: L.layerGroup(),
-        data: [],
-        color: '#f78c6b'
-    },
-    "Großflächen": {
-        group: L.layerGroup(),
-        data: [],
-        color: '#ffd166'
-    },
-    "Stromkasten": {
-        group: L.layerGroup(),
-        data: [],
-        color: '#06d6a0' 
-    },
-    "Litfaßsäule": {
-        group: L.layerGroup(),
-        data: [],
-        color: '#118ab2' 
-    },
-    "Plakatwerbung": {
-        group: L.layerGroup(),
-        data: [],
-        color: '#073b4c' 
-    }
+    "City-Light-Poster": { group: L.layerGroup(), color: '#54478c'},
+    "City-Light-Board": { group: L.layerGroup(), color: '#2c699a'},
+    "City Light Säule": { group: L.layerGroup(), color: '#048ba8' },
+    "Fassadenwerbung": { group: L.layerGroup(), color: '#0db39e' },
+    "Großflächen": { group: L.layerGroup(), color: '#16db93'},
+    "Leuchtkasten": { group: L.layerGroup(), color: '#83e377' },
+    "Litfaßsäule": { group: L.layerGroup(), color: '#b9e769' },
+    "Mastenschild": { group: L.layerGroup(), color: '#efea5a' },
+    "Plakatwerbung": { group: L.layerGroup(), color: '#f1c453' },
+    "Stromkasten": { group: L.layerGroup(), color: '#f29e4c' },
+    "Uhrenwerbung": { group: L.layerGroup(),color: '#f694c1' }
 };
 
 // Definieren Sie die Funktion zur Erstellung der benutzerdefinierten Schichtenkontrolle
@@ -115,24 +109,45 @@ for (var groupName in layerGroups) {
     layerGroups[groupName].group.addTo(map);
     options[groupName] = { group: layerGroups[groupName].group, color: layerGroups[groupName].color};
 }
-var customLayerControl = L.control.customLayerGroups(options, {position: 'topright', className: 'custom-control-input'});
+var customLayerControl = L.control.customLayerGroups(options, {position: 'top right', className: 'custom-control-input'});
+customLayerControl.setPosition('bottomright');
 map.addControl(customLayerControl);
 
+/* Werbetraeger Daten von Github ziehen */
 
-fetch('https://raw.githubusercontent.com/falkzoo/map_prototype/main/standort_daten.json')
+fetch('https://raw.githubusercontent.com/falkzoo/map_prototype/main/geojson/standort_daten.json')
     .then(response => response.json())
     .then(data => {
         for (let groupName in data) {
             if (groupName in layerGroups) {
-                layerGroups[groupName].data = packagePoints(data[groupName])
-                layerGroups[groupName].group.addLayer(layerGroups[groupName].data)
+                layerGroups[groupName].group.addLayer(packagePoints(data[groupName]))
             }
         }
     })
     .catch(function(error) {
-        console.log(`This is the error: ${error}`)
+        console.log(`Error processing standort data: ${error}`)
     })
 
+/* Bezirks Info Feld */
+
+var info = L.control();
+
+info.onAdd = function (map) {
+    this._div = L.DomUtil.create('div', 'info'); // create a div with a class "info"
+    this.update();
+    return this._div;
+};
+
+// method that we will use to update the control based on feature properties passed
+info.update = function (props) {
+    this._div.innerHTML = '<h4>Bezirk:</h4>' +  (props ?
+        '<b>' + props.name + '</b><br />' + "Anzahl der Werbeträger: "
+        : 'Hover über einen Bezirk');
+};
+
+info.addTo(map);
+
+/* Funktionen */
 
 function packagePoints(collection) {
     var geoJsonLayer = new L.geoJSON(collection, {
@@ -140,14 +155,7 @@ function packagePoints(collection) {
         return L.circleMarker(latlng, geoJsonMarkerOptions);
     },
     style: function(feature) {
-        switch(feature.properties.Category) {
-            case "City-Light-Poster": return { color : '#ef476f' };
-            case "City-Light-Board": return { color : '#f78c6b' };
-            case 'Großflächen': return { color : '#ffd166' };
-            case 'Stromkasten': return { color : '#06d6a0' };
-            case 'Litfaßsäule': return { color : '#118ab2' };
-            case 'Plakatwerbung': return { color : '#073b4c'  };
-        }
+        return { fillColor : layerGroups[feature.properties.Category].color };
     },
     onEachFeature: function(feature, layer){
         if (feature.properties && feature.properties.popupContent) {
@@ -159,4 +167,50 @@ function packagePoints(collection) {
     });
 
     return geoJsonLayer
+}
+
+function packageBezirke(collection) {
+    var geoJsonLayer = new L.geoJSON(collection, {
+        style: function() {
+            return {
+                color: 'darkgreen',
+                opacity: 1,
+                fillOpacity: 0,
+                weight: 2,
+                dashArray: '10'
+            }
+        },
+        onEachFeature: function(feature, layer) {
+            layer.on({
+                mouseover: highlightFeature,
+                mouseout: resetHighlight,
+                click: zoomToFeature
+            });
+        }
+    })
+
+    return geoJsonLayer
+}
+
+function highlightFeature(e) {
+    var layer = e.target;
+
+    layer.setStyle({
+        weight: 5,
+        color: '#666',
+        dashArray: '',
+        fillOpacity: 0.2
+    });
+
+    info.update(layer.feature.properties);
+    layer.bringToBack();
+}
+
+function resetHighlight(e) {
+    geojsonBezirke.resetStyle(e.target);
+    info.update();
+}
+
+function zoomToFeature(e) {
+    map.fitBounds(e.target.getBounds());
 }
